82bb91e0c3cd74d4aeabf6fb640e8a486b81a548
DeepakNaik <deepaknaikofcl@gmail.com>
2025-01-18 14:57:33 +0530

    first commit

---
+++ controller/StudentMarksController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.StudentMarks;
+import com.example.demo.service.StudentMarksService;
+
+@RestController
+@CrossOrigin("*")
+public class StudentMarksController {
+	
+	@Autowired
+	StudentMarksService stuMarksService;
+	
+	@PostMapping("/exam/marks/add/all")
+	public String addAllNewStudentMarks(@RequestBody List<StudentMarks> allStuMarksList){
+		
+		
+		String res = stuMarksService.addAllNewStudentMarks(allStuMarksList);
+		
+		return res;
+	}
+	
+	@GetMapping("/exam/marks/60")
+	public List<StudentMarks>  getAll60AboveMarks(){
+		
+		return stuMarksService.fetchAllStudentsAbove60Marks();
+		
+		
+	}
+	
+
+}
---
+++ service/StudentMarksService.java
@@ -0,0 +1,57 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.StudentMarks;
+import com.example.demo.repo.StudentMarksRepo;
+
+@Service
+public class StudentMarksService {
+	
+	@Autowired
+	StudentMarksRepo stuMarksRepo;
+
+	public String addAllNewStudentMarks(List<StudentMarks> allStuMarksList) {
+		String res = "";
+		
+		try {
+			stuMarksRepo.saveAll(allStuMarksList);
+			res = "successfully added all students marks";
+		}catch(Exception e) {
+			res = "not able to save into DB";
+		}
+		return res;
+	}
+
+	public List<StudentMarks> fetchAllStudentsAbove60Marks() {
+		
+		
+		List<StudentMarks> allStudents = stuMarksRepo.findAll();
+		
+		StudentMarks eachStu = null;
+		
+		List<StudentMarks> all60MarksStusList = new ArrayList<StudentMarks>();
+		
+		for(int i=0;i<allStudents.size();i++) {
+			
+			eachStu = allStudents.get(i);
+			
+			if( (eachStu.getMaths() >= 60) 
+					&& (eachStu.getSci() >= 60)
+					&&(eachStu.getEng() >= 60)
+				) {
+				
+				all60MarksStusList.add(eachStu);
+			}
+			
+		}
+		
+		
+		return all60MarksStusList;
+	}
+
+}
